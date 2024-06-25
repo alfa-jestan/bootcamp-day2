15aa4a7d4b504ff38f643052bc1e9cc65b234bad
alfa-jestan <tmbt.jestan@gmail.com>
2024-06-25 19:40:36 +0200

    added way to display list from backend to frontend

---
+++ src/day2_frontend/src/components/Blog.vue
@@ -0,0 +1,24 @@
+<template>
+    <div>
+        <button @click="Pobierzwpisy">refresh</button>
+        pozdrawia ambrorzy co dzieci tworzy
+        {{ wpisy }}
+    </div>
+</template>
+
+<script>
+import { day2_backend } from 'declarations/day2_backend/index';
+export default {
+    data() {
+        return {
+            wpisy: []
+        }
+    },
+    methods: {
+        async Pobierzwpisy() {
+            this.wpisy = await day2_backend.odczytaj_wpisy()
+            
+        }
+    }
+} 
+</script>
