ddfd5fef3a0e66e037c5843cf8144508ccbe02f6
alfa-jestan <tmbt.jestan@gmail.com>
2024-06-25 19:55:35 +0200

    added way to append our list from backend

--- src/day2_frontend/src/components/Blog.vue
+++ src/day2_frontend/src/components/Blog.vue
@@ -3,6 +3,8 @@
         <button @click="Pobierzwpisy">refresh</button>
         pozdrawia ambrorzy co dzieci tworzy
         {{ wpisy }}
+        <input v-model="nowyBlog" type="text">
+        <button @click="dodajWpis">dodaj</button>
     </div>
 </template>
 
@@ -11,13 +13,16 @@ import { day2_backend } from 'declarations/day2_backend/index';
 export default {
     data() {
         return {
-            wpisy: []
+            wpisy: [],
+            nowyBlog: ""
         }
     },
     methods: {
+        async dodajWpis() {
+            await day2_backend.dodaj_wpis(this.nowyBlog);
+        },
         async Pobierzwpisy() {
-            this.wpisy = await day2_backend.odczytaj_wpisy()
-            
+            this.wpisy = await day2_backend.odczytaj_wpisy();          
         }
     }
 } 
