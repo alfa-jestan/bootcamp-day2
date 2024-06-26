9346b6ee411ac62ea4e535f0071b4ee151a7112e
alfa-jestan <tmbt.jestan@gmail.com>
2024-06-26 19:16:52 +0200

    style css

--- src/day2_frontend/src/components/Blog.vue
+++ src/day2_frontend/src/components/Blog.vue
@@ -1,10 +1,19 @@
 <template>
     <div>
-        <button @click="Pobierzwpisy">refresh</button>
+        <h1 class="text-blue-600">wpisuj dane szczylu</h1>
+        <div class="w-100 flex flex-row-reverse">
+            <button @click="Pobierzwpisy" class="float-right bg-blue-600 rounded text-white p-10">refresh</button>
+        </div>
         pozdrawia ambrorzy co dzieci tworzy
-        {{ wpisy }}
-        <input v-model="nowyBlog" type="text">
-        <button @click="dodajWpis">dodaj</button>
+        <div class="grid mx-6 gap-4 my-4">
+            <div v-for="wpis in wpisy" class="drop-shadow-xl bg-stone-300 p-4">
+                <p>{{ wpis }}</p>
+            </div>
+        </div>
+        <div class="flex justify-center flex-col">
+            <input v-model="nowyBlog" type="text" class="border-2 border-blue-400 p-4">
+            <button @click="dodajWpis" class="bg-blue-600 rounded text-white p-10">dodaj</button>
+        </div>
     </div>
 </template>
 
@@ -24,6 +33,9 @@ export default {
         async Pobierzwpisy() {
             this.wpisy = await day2_backend.odczytaj_wpisy();          
         }
+    },
+    async mounted() {
+        this.Pobierzwpisy()
     }
 } 
 </script>
